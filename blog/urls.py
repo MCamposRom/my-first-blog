82f09e23bc3768dc66a60db4cc3bbeabe3e95471
MCamposRom <mcmaposrom@educacion.navarra.es>
2019-10-16 21:55:56 +0000

    Funciona en vivo!

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
+#from django.conf.urls import url
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
+app_name='blog'
+
 urlpatterns = [
-    url(r'^$', views.post_list, name='post-list'),
-    #path('', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post-detail'),
-    #path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #url(r'^$', views.post_list, name='post-list'),
+    path('', views.post_list, name='post-list'),
+    #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post-detail'),
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),
     #url(r'^post/new/$', views.post_new, name='post_new'),
 ]
