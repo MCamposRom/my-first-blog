554428d6f651537a9f5d09878f0db832787f73ad
MCamposRom <mcamposrom@educacion.navarra.es>
2019-10-17 01:09:42 +0200

    fin_tutorial_local

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', views.post_list, name='post-list'),
     #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post-detail'),
     path('post/<int:pk>/', views.post_detail, name='post-detail'),
-    #url(r'^post/new/$', views.post_new, name='post_new'),
+    #url(r'^post/new/$', views.post_new, name='post-new'),
+    path('post/new', views.post_new, name='post-new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post-edit'),
 ]
