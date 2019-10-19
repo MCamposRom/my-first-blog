5602e6be6abaa6200966d3a3e6778c4a09566d0e
MCamposRom <mcamposrom@educacion.navarra.es>
2019-10-19 13:02:29 +0200

    Editando_blog

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import login as do_login
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -45,7 +45,7 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 @login_required
